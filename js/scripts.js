7c37a202a6fcc77324a29f33bb1e72af8f153676
ar2849 <49045825+ar2849@users.noreply.github.com>
2019-05-16 15:39:05 -0400

    summary

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,13 @@ map.on('style.load', function() {
   function(data) {
   data.features.map(function(feature) {
   feature.properties.p_diagnoses = parseInt(feature.properties.p_diagnoses);
+  feature.properties.p_population = parseInt(feature.properties.p_population);
+  feature.properties.p_sexed = parseInt(feature.properties.p_sexed);
+  feature.properties.p_hived = parseInt(feature.properties.p_hived);
+  feature.properties.p_inform6 = parseInt(feature.properties.p_inform6);
+  feature.properties.p_inform9 = parseInt(feature.properties.p_inform9);
+  feature.properties.p_hiv6 = parseInt(feature.properties.p_hiv6);
+  feature.properties.p_hiv9 = parseInt(feature.properties.p_hiv9);
   });
  };
 //colors the map with the percentages of HIV infection per state
@@ -39,4 +46,47 @@ map.on('style.load', function() {
   ],
  }
  });
+
+ map.addLayer({
+ id: 'highlight',
+ type: 'line',
+ source: 'webmap',
+ paint: {
+      'line-width': 1,
+      'line-opacity': 0.7,
+      'line-color': 'white',
+    }
+});
+
+map.on('mousemove', function (e) {
+  // query for the features under the mouse, but only in the lots layer
+  var features = map.queryRenderedFeatures(e.point, {
+      layers: ['p_diagnoses'],
+  });
+
+  // get the first feature from the array of returned features.
+  var webmap = features[0]
+
+  if (webmap) {  // if there's a lot under the mouse, do stuff
+    map.getCanvas().style.cursor = 'pointer';  // make the cursor a pointer
+
+    // lookup the corresponding description for the land use code
+    var landuseDescription = LandUseLookup(parseInt(lot.properties.landuse)).description;
+
+    // use jquery to display the address and land use description to the sidebar
+    $('#p_population').text(lot.properties.population);
+    $('#p-diagnoses').text(numberofhivdiagnoses);
+
+    // set this lot's polygon feature as the data for the highlight source
+    map.getSource('highlight').setData(lot.geometry);
+  } else {
+    map.getCanvas().style.cursor = 'default'; // make the cursor default
+
+    // reset the highlight source to an empty featurecollection
+    map.getSource('highlight').setData({
+      type: 'FeatureCollection',
+      features: []
+    });
+  }
+})
 });
